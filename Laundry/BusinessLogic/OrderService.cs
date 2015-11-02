327219080f9ad65f4a73443753c8f725327eecf0
Adam Szady <adam.szady@outlook.com>
2015-11-02 18:20:40 +0100

    nothing

---
+++ Laundry/BusinessLogic/OrderService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace BusinessLogic
+{
+    public class OrderService
+    {
+        private LaundryEntities DBContext;
+        public OrderService()
+        {
+            DBContext = new LaundryEntities();
+        }
+        public OrderService(LaundryEntities dbContext)
+        {
+            this.DBContext = dbContext;
+        }
+
+
+    }
+}
---
+++ Laundry/LaundryDatabase/OrderParts.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[OrderParts]
+(
+	[OrderId] INT NOT NULL FOREIGN KEY REFERENCES Orders(Id), 
+    [PriceId] INT NULL, 
+    [Number] INT NULL DEFAULT 1,
+)
