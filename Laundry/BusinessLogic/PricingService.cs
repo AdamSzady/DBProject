bdf90d0a837af7389c57a045043f36264e47f67e
Norbert Songin <wezyrnorbert@gmail.com>
2015-11-16 14:54:12 +0100

    Added Pricing page. Some style changes.

---
+++ Laundry/BusinessLogic/PricingService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using DataAccess;
+using DTO.Models;
+
+namespace BusinessLogic
+{
+    public class PricingService
+    {
+        private LaundryEntities dbContext;
+
+        public PricingService()
+        {
+            dbContext = new LaundryEntities();
+        }
+
+        public IEnumerable<PricesDTO> GetPricesList()
+        {
+            Mapper.CreateMap<IEnumerable<Prices>, IEnumerable<PricesDTO>>();
+            return dbContext.Things.Select(c => new PricesDTO { Id = c.Id }).ToList();
+        }
+    }
+}
---
+++ Laundry/Laundry/Controllers/PricingController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Laundry.Models;
+using BusinessLogic;
+using DTO.Models;
+
+namespace Laundry.Controllers
+{
+    public class PricingController : Controller
+    {
+
+        private readonly PricingService pricingService;
+
+        public PricingController()
+        {
+            this.pricingService = new PricingService();
+        }
+
+        public PricingController(PricingService pricingService)
+        {
+            this.pricingService = pricingService;
+        }
+
+        // GET: Prices
+        public ActionResult Index()
+        {
+            Pricing model = new Pricing
+            {
+                Prices = pricingService.GetPricesList()         
+            };
+            return View(model);
+        }
+    }
+}
