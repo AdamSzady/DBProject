a4df69357dd672a84485303a0df5875fa025453c
Adam Szady <adam.szady@outlook.com>
2015-11-17 00:06:14 +0100

    Posibility to adding prices.

---
+++ Laundry/DTO/Models/Price.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO.Models
+{
+    public class Price
+    {
+        public int PriceId { get; set; }
+        public string ThingName { get; set; }
+        public string ServiceName { get; set; }
+        public float? Value { get; set; }
+    }
+}
