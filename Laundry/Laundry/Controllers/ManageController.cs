558a32ce512b3492f063f1073d495e9f99e0f953
Norbert Songin <wezyrnorbert@gmail.com>
2015-11-29 01:29:00 +0100

    Account management, registration and login page styling.

--- Laundry/Laundry/Controllers/ManageController.cs
+++ Laundry/Laundry/Controllers/ManageController.cs
@@ -55,10 +55,10 @@ namespace Laundry.Controllers
         public async Task<ActionResult> Index(ManageMessageId? message)
         {
             ViewBag.StatusMessage =
-                message == ManageMessageId.ChangePasswordSuccess ? "Your password has been changed."
+                message == ManageMessageId.ChangePasswordSuccess ? "Hasło zostało zmienione."
                 : message == ManageMessageId.SetPasswordSuccess ? "Your password has been set."
                 : message == ManageMessageId.SetTwoFactorSuccess ? "Your two-factor authentication provider has been set."
-                : message == ManageMessageId.Error ? "An error has occurred."
+                : message == ManageMessageId.Error ? "Wystąpił błąd."
                 : message == ManageMessageId.AddPhoneSuccess ? "Your phone number was added."
                 : message == ManageMessageId.RemovePhoneSuccess ? "Your phone number was removed."
                 : "";
